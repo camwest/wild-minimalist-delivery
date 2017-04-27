15c17134c64b8785ae663fd5ea45be37709e24be
Cameron Westland <camwest@gmail.com>
2017-04-27 05:59:19 -0700

    add some initial styling

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,28 +1,98 @@
 import * as React from 'react';
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
-  font-size: 1.25em;
-  font-family: courier;
-  padding: 0.5em;
-  margin: 0.5em;
+const atoms = {
+  colors: {
+    one: '#3F51B5'
+  },
+
+  fonts: {
+    default: 'courier'
+  }
+};
+
+interface FocusProps extends React.HTMLAttributes<HTMLDivElement> {
+  focus: boolean;
+  children?: any;
+}
+
+const Focusable: React.SFC<FocusProps> = (props: any) => (
+  <div {...props}>{props.children}</div>
+);
+
+const Wrapper = styled(Focusable) `
+  background: ${props => props.focus ? 'lightgrey' : 'transparent'};;
+  box-sizing: border-box;
+  color: ${atoms.colors.one};
+  font-family: ${atoms.fonts.default};
+  font-size: 14px;
+  line-height: 36px;
+  padding-left: 12.5px;
+  padding-top: 8px;
+  padding-bottom: 10px;
 `;
 
-export interface Props {
+const Input = styled.input`
+  background: transparent;
+  margin-left: 12.5px;
+  font-size: 14px;
+  line-height: 36px;
+  border-width: 0;
+  padding: 0;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+interface Props {
   children?: any;
   placeholder?: string;
   type?: string;
 }
 
-const TextInput: React.SFC<Props> = ({ children, placeholder, type }: Props) => (
-  <Wrapper>
-    {placeholder}
-    <input type={type} />
-  </Wrapper>
-);
+interface State {
+  focus: boolean;
+}
 
-TextInput.defaultProps = {
-  type: 'text'
-};
+class TextInput extends React.Component<Props, State> {
+  static defaultProps = {
+    type: 'text'
+  };
+
+  private input: HTMLInputElement;
+
+  constructor(props: Props) {
+    super(props);
+    this.state = { focus: false };
+  }
+
+  handleFocus = () => {
+    this.setState(() => {
+      return { focus: true };
+    });
+  }
+
+  handleBlur = () => {
+    this.setState(() => {
+      return { focus: false };
+    });
+  }
+
+  handleClick = () => {
+    this.input.focus();
+  }
+
+  render() {
+    const { placeholder, type } = this.props;
+
+    return (
+      <Wrapper focus={this.state.focus} onClick={this.handleClick}>
+        {placeholder}
+        <Input innerRef={r => this.input = r} type={type} onFocus={this.handleFocus} onBlur={this.handleBlur} />
+      </Wrapper>
+    );
+  }
+}
 
 export default TextInput;
