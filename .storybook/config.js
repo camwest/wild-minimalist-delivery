8c1cb44a395af6153df6dd6533f782a3e69641ba
Cameron Westland <camwest@gmail.com>
2017-04-28 06:15:51 -0700

    add storybook example addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,12 @@
-import { configure } from '@kadira/storybook';
+import { configure, addDecorator } from '@kadira/storybook';
+
+import WithExample from './addons/example-addon';
+import '../src/index.css';
 
 function loadStories() {
   require('../src/stories');
 }
+
+addDecorator(WithExample);
 
 configure(loadStories, module);
